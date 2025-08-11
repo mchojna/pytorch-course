d17ce98f05224996c332f4bfa637103ce275cb54
Michał Chojna <93717130+mchojna@users.noreply.github.com>
2025-08-11 13:44:07 +0200

    Added predict function

--- 05_pytorch_going_modular_exercises/src/utils.py
+++ 05_pytorch_going_modular_exercises/src/utils.py
@@ -4,6 +4,7 @@ File containint utility functions for model training
 
 from pathlib import Path
 import torch
+import model_builder
 
 def save_model(
     model: torch.nn.Module,
@@ -32,3 +33,30 @@ def save_model(
     print(f"[INFO] Saving model to: {model_save_path}")
     torch.save(obj=model.state_dict(),
                 f=model_save_path)
+
+
+def load_model(
+    input_shape: int,
+    hidden_units: int,
+    output_shape: int,
+    model_path: str,
+) -> torch.nn.Module:
+    """Loades a PyTorch model from a target directory.
+
+    Args:
+        input_shape (int): Input shape.
+        hidden_units (int): Number of hidden units.
+        output_shape (_type_): Output shape.
+
+    Returns:
+        torch.nn.Module: PyTorch module.
+    """
+
+    model = model_builder.TinyVGG(
+        input_shape=input_shape,
+        hidden_units=hidden_units,
+        output_shape=output_shape,
+    )
+    model.load_state_dict(torch.load(model_path, weights_only=True))
+
+    return model
