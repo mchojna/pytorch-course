ad4d1c3e3e89b6616529e040b2393cc12cc1b563
Michał Chojna <93717130+mchojna@users.noreply.github.com>
2025-08-18 14:19:54 +0200

    Started experiments

--- 07_pytorch_experiment_tracking/src/utils.py
+++ 07_pytorch_experiment_tracking/src/utils.py
@@ -4,7 +4,8 @@ File containint utility functions for model training
 
 from pathlib import Path
 import torch
-import model_builder
+# import model_builder
+
 
 def save_model(
     model: torch.nn.Module,
@@ -35,28 +36,28 @@ def save_model(
                 f=model_save_path)
 
 
-def load_model(
-    input_shape: int,
-    hidden_units: int,
-    output_shape: int,
-    model_path: str,
-) -> torch.nn.Module:
-    """Loades a PyTorch model from a target directory.
+# def load_model(
+#     input_shape: int,
+#     hidden_units: int,
+#     output_shape: int,
+#     model_path: str,
+# ) -> torch.nn.Module:
+#     """Loades a PyTorch model from a target directory.
 
-    Args:
-        input_shape (int): Input shape.
-        hidden_units (int): Number of hidden units.
-        output_shape (_type_): Output shape.
+#     Args:
+#         input_shape (int): Input shape.
+#         hidden_units (int): Number of hidden units.
+#         output_shape (_type_): Output shape.
 
-    Returns:
-        torch.nn.Module: PyTorch module.
-    """
+#     Returns:
+#         torch.nn.Module: PyTorch module.
+#     """
 
-    model = model_builder.TinyVGG(
-        input_shape=input_shape,
-        hidden_units=hidden_units,
-        output_shape=output_shape,
-    )
-    model.load_state_dict(torch.load(model_path, weights_only=True))
+#     model = model_builder.TinyVGG(
+#         input_shape=input_shape,
+#         hidden_units=hidden_units,
+#         output_shape=output_shape,
+#     )
+#     model.load_state_dict(torch.load(model_path, weights_only=True))
 
-    return model
+#     return model
